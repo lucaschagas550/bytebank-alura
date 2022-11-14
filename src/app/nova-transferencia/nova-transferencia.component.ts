79f9a2f1f09b35186814aaf06277320f2963b628
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-11-14 17:51:02 -0300

    feat: primeiro componente

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'nova-transferencia',
+    templateUrl: './nova-transferencia.component.html',
+    styleUrls: ['./nova-transferencia.component.css']
+})
+
+export class NovaTransferenciaComponent{
+
+}
