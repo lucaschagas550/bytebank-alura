354d0da2f3cee9e82b5db4ffce2e8f6dfe69841d
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-11-14 18:45:03 -0300

    feat: Estilizando o formulario

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-nova-transferencia', // obrigatorio ter o 'app' porque esta configurado no arquivo angular.json
     templateUrl: './nova-transferencia.component.html',
-    styleUrls: ['./nova-transferencia.component.css']
+    styleUrls: ['./nova-transferencia.component.scss']
 })
 
 export class NovaTransferenciaComponent{
