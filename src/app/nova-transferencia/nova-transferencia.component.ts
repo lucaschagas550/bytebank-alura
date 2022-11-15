47244f5d09c0f8dc6af9d61f69c99c0aabd94130
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-11-15 15:14:24 -0300

    feat: exportando dados com output

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector: 'app-nova-transferencia', // obrigatorio ter o 'app' porque esta configurado no arquivo angular.json
@@ -7,11 +7,15 @@ import { Component } from "@angular/core";
 })
 
 export class NovaTransferenciaComponent{
+    @Output() aoTransferir = new EventEmitter<any>(); //evento para saida de dados
+
   valor: number;
   destino: number;
 
   transferir(){
-    console.log('Solicitada nova transferência valor: ' + this.valor +
-     ' para o destino: ' + this.destino);
+    console.log('Solicitada nova transferência valor: ' + this.valor +' para o destino: ' + this.destino);
+
+    const valorEmitir = {valor: this.valor, destino: this.destino}; //criando um objeto para receber o valor e o destino
+    this.aoTransferir.emit(valorEmitir);
   }
 }
