af5f62c4d6a15863378cc0fb51be82f6511d676b
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-11-15 19:14:13 -0300

    feat: Melhorando a visualizacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ExtratoComponent } from './extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt');
 
 //Index Html => AppComponent => AppModule
 //Declarar novos componentes
@@ -18,7 +23,10 @@ import { ExtratoComponent } from './extrato/extrato.component';
     BrowserModule,
     FormsModule //Modulo de formulario, autoimport por causa das extenções instaladas
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt-br'}, // Hora padrao para o aplicativo
+    {provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL'} //Moeda padrao para o aplicativo
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
